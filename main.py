665665f1d21e0b7ccc57ccddb3ef8bdb48ef8fcf
albertoh88 <albertoh8806@gmail.com>
2025-09-30 13:40:05 -0300

    fix: correções em testes, conexão DB e envio de email

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from routers import router
 from fastapi import FastAPI
+from __version__ import __version__
 
 app = FastAPI()
 
@@ -9,7 +10,7 @@ app.include_router(router.router)
 
 @app.get('/')
 def read_root():
-    return {"message": "Este es el primer endpoint"}
+    return {"message": "Este es el primer endpoint", 'Version': __version__}
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
